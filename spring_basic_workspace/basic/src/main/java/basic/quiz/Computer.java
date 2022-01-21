3f5212c72a7c9aff60d59def61bb980f55129552
tpleehan <tpleehan@gmail.com>
2022-01-21 19:08:59 +0900

    commit

--- spring_basic_workspace/basic/src/main/java/basic/quiz/Computer.java
+++ spring_basic_workspace/basic/src/main/java/basic/quiz/Computer.java
@@ -13,11 +13,23 @@ public class Computer {
 	@Autowired
 	private Mouse mouse;
 	
+	/*
+	예시2번) - 생성자를 통한 어노테이션
+	@Autowired
+	public Computer(Monitor monitor, Keyboard keyboard, Mouse mouse) {
+		super();
+		this.monitor = monitor;
+		this.keyboard = keyboard;
+		this.mouse = mouse;
+	}
+	*/
+
 	public void computerInfo() {
+		System.out.println("*** 컴퓨터의 정보 ***");
+		System.out.println("삼성 컴퓨터");
 		monitor.info();
 		keyboard.info();
 		mouse.info();
-		System.out.println("삼성 컴퓨터");
 		
 	}
 }
